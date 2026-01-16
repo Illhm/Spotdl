865ee8f74478cb8dd62674bb9a2b7ff3d37146fe
Illhm <gusmif@ilhamxander.web.id>
2026-01-16 20:24:03 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
-import requests
+import requests 
 import uvicorn 
 from typing import Optional
 import re
